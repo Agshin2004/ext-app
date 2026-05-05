8eec15b743fb331e76168329e81a0abe0eea1f3e
agshin2004 <nadirov.main@gmail.com>
2026-05-05 22:36:27 +0400

    feat(cache): add dependencies and config

---
+++ src/main/java/com/agshin/extapp/shared/config/CacheConfig.java
@@ -0,0 +1,34 @@
+package com.agshin.extapp.shared.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(
+                "categories",
+                "expenses"
+        );
+
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .maximumSize(500)
+                        .expireAfterWrite(Duration.ofMinutes(60))
+                        .recordStats()
+        );
+
+        return cacheManager;
+    }
+
+
+}
